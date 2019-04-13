895403df6a75684b8bf499271ad9eaec073520a3
Never04eK <heket844@gmail.com>
2019-04-13 23:34:09 +0300

    Add eyeEvent

---
+++ EducationARProject/Assets/OnEyeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class OnEyeScript : MonoBehaviour
+{
+    [Tooltip("Rotation object to edit")]
+    public Rotation rotation;
+
+    public GameObject placeOfInterest;
+    public GameObject pointer;
+
+
+
+    public void OnEyeFocusDwell()
+    {
+        Debug.Log("Jopa");
+    }
+}
