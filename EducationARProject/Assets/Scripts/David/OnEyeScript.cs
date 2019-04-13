425cd1c6d64aaef44117cf195afad37bc9aa1dcb
Un1VersE <gamaundavid23@gmail.com>
2019-04-14 02:01:58 +0300

    Merged versions

---
+++ EducationARProject/Assets/Scripts/David/OnEyeScript.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+namespace Assets.Scripts.David
+{
+    public class OnEyeScript : MonoBehaviour, IMixedRealityFocusHandler
+    {
+
+        public Animator Anim;
+
+        void IMixedRealityFocusHandler.OnFocusEnter(FocusEventData eventData)
+        {
+            Debug.Log("Jopa");
+            Anim.SetBool("focused" , true);
+        }
+
+        void IMixedRealityFocusHandler.OnFocusExit(FocusEventData eventData)
+        {
+            Debug.Log("Hui");
+            Anim.SetBool("focused", false);
+        }
+    }
+}
