0cefafb7fbfec6a91fed446cf40835ee6bbf0c0b
Un1VersE <gamaundavid23@gmail.com>
2019-04-13 20:54:13 +0300

    Add Earth and other stuff
    jopa

---
+++ EducationARProject/Assets/Rotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+  
+    public Transform earthTransform;
+    public float rotationSpeed = -50f;
+    public bool isRotationActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRotationActive)
+        {
+            earthTransform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+
+    }
+}
